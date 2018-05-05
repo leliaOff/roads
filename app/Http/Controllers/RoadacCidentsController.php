c31233e470fbadd014a5a25410245a3ace09e976
Xarrper <xarrper@gmail.com>
2018-05-05 14:55:20 +0400

    .

--- app/Http/Controllers/RoadacCidentsController.php
+++ app/Http/Controllers/RoadacCidentsController.php
@@ -6,24 +6,37 @@ use Illuminate\Http\Request;
 
 class RoadacCidentsController extends Controller
 {
-    private $date = [ 
-        { 
-        "repair_kind": "Ремонт", 
-        "date_start": "", 
-        "date_end": "", 
-        "date_start_plan": "20180301", 
-        "date_end_plan": "20180301", 
-        "start_latitude": "", 
-        "start_longitude": "", 
-        "end_latitude": "", 
-        "end_longitude": "", 
-        "reason": "Не определена", 
-        "trouble_type": "Не определен" 
-        } 
-    ]
 
     public function getData() {
-        dd($this->date);
-        return $this->date;
+        return $this->mockeData();
+    }
+
+    private function mockeData() {
+        return [(object)[ 
+            "repair_kind"=> "Ремонт", 
+            "date_start"=> "", 
+            "date_end"=> "", 
+            "date_start_plan"=> "20180301", 
+            "date_end_plan"=> "20180301", 
+            "start_latitude"=> "", 
+            "start_longitude"=> "", 
+            "end_latitude"=> "", 
+            "end_longitude"=> "", 
+            "reason"=> "Не определена", 
+            "trouble_type"=> "Не определен" 
+        ],
+        (object)[ 
+            "repair_kind"=> "Ремонт", 
+            "date_start"=> "", 
+            "date_end"=> "", 
+            "date_start_plan"=> "20180301", 
+            "date_end_plan"=> "20180301", 
+            "start_latitude"=> "", 
+            "start_longitude"=> "", 
+            "end_latitude"=> "", 
+            "end_longitude"=> "", 
+            "reason"=> "Не определена", 
+            "trouble_type"=> "Не определен" 
+        ]];
     }
 }
